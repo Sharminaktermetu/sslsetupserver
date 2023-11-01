d297887440d283af94147b7cfef36be593753c60
Sharminaktermetu <sharminacc1@gmail.com>
2023-11-01 12:19:12 +0600

    ssl update

--- index.js
+++ index.js
@@ -21,17 +21,14 @@ app.get('/', async (req, res) => {
       
     })
   })
-//sslcommerz init
-// app.post('/ssl-request', (req, res) => {
-//   console.log(req.body);
-// })
+
 app.post('/ssl-request', (req, res) => {
   
     const data = {
         total_amount: 100,
         currency: 'BDT',
         tran_id: 'REF123', // use unique tran_id for each api call
-        success_url: `https://ssl-payment.onrender.com/ssl-payment-success`,
+        success_url: `https://setup-2dac0.web.app/payment-success`,
         fail_url: 'http://localhost:3030/fail',
         cancel_url: 'http://localhost:3030/cancel',
         ipn_url: 'http://localhost:3030/ipn',
@@ -75,9 +72,9 @@ app.post('/ssl-request', (req, res) => {
 
 })
 
-  app.post("/ssl-payment-success", async (req, res) => {
+  app.post("/payment-success", async (req, res) => {
 
-    return res.redirect('https://setup-2dac0.web.app/ssl-payment-success')
+    return res.redirect('https://setup-2dac0.web.app/payment-success')
   })
 
 app.listen(port, () => {
